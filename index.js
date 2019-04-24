4bdfe79d864946efaa1ac1d2bee2e0867d87a2cc
Erkkapyy <erkka.pyykonen@gmail.com>
2019-04-24 19:33:57 +0300

    more heroku

--- index.js
+++ index.js
@@ -2,22 +2,18 @@ const mongoose = require("mongoose")
 const { ApolloServer, UserInputError, gql } = require("apollo-server")
 const jwt = require("jsonwebtoken")
 const { createApolloFetch } = require("apollo-fetch")
-const geocode = require("./apis/rest/geocode")
-const http = require("http")
 const cors = require("cors")
-const PORT = process.env.PORT || 3003
+const express = require("express")
+const app = express()
 
-const httpServer = http.createServer(geocode)
-httpServer.listen(PORT, () => {
-  console.log("http geocode server running on port 3003")
-})
+const PORT = process.env.PORT || 3003
 
 const HSL_ROUTE_API_URI =
   "https://api.digitransit.fi/routing/v1/routers/next-hsl/index/graphql"
 
 const fetch = new createApolloFetch({ uri: HSL_ROUTE_API_URI })
 
-//app.use(cors())
+app.use(cors())
 console.log("connecting to", HSL_ROUTE_API_URI)
 
 const typeDefs = gql`
@@ -149,5 +145,8 @@ const resolvers = {
     }
   }
 }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("/build"))
+}
 const server = new ApolloServer({ typeDefs, resolvers })
-server.listen().then(({ url }) => console.log("sever ready at", url))
+server.listen().then(({ url }) => console.log("server ready at", url))
