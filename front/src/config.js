a2badf0c0733c1ccbfec20ab0a66c455fd84392c
Erkkapyy <erkka.pyykonen@gmail.com>
2019-04-26 17:06:19 +0300

    more heroku

--- front/src/config.js
+++ front/src/config.js
@@ -5,6 +5,10 @@ if (process.env.NODE_ENV !== "production") {
 let WSPORT = process.env.REACT_APP_WEBSOCKET_PORT
 let PORT = process.env.REACT_APP_HTTP_PORT
 
+if (process.env.NODE_ENV === "production") {
+  PORT = process.env.PORT
+}
+
 module.exports = {
   WSPORT,
   PORT
