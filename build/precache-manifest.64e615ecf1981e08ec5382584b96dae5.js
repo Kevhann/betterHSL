b09b3f802445fda520494ef22ffb8f6bf3701f40
Erkkapyy <erkka.pyykonen@gmail.com>
2019-04-26 17:21:25 +0300

    heroku pls work

---
+++ build/precache-manifest.64e615ecf1981e08ec5382584b96dae5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4aa969f52e2d09f79fdb",
+    "url": "/static/js/main.0ef7efbc.chunk.js"
+  },
+  {
+    "revision": "58819d47ecf954792a94",
+    "url": "/static/js/2.155780f9.chunk.js"
+  },
+  {
+    "revision": "4aa969f52e2d09f79fdb",
+    "url": "/static/css/main.d2cb3b77.chunk.css"
+  },
+  {
+    "revision": "58819d47ecf954792a94",
+    "url": "/static/css/2.cd30f7ef.chunk.css"
+  },
+  {
+    "revision": "18c946dea8d4cc889b6a4cf55a743b6e",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.d143b8ee6492a0f898bd731ef5d3523f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "107a9f5516edd6230d9d",
+    "url": "/static/js/main.c1f235b4.chunk.js"
+  },
+  {
+    "revision": "db305a26f8a41c91ef1d",
+    "url": "/static/js/2.b86afdb8.chunk.js"
+  },
+  {
+    "revision": "107a9f5516edd6230d9d",
+    "url": "/static/css/main.e6ffb529.chunk.css"
+  },
+  {
+    "revision": "db305a26f8a41c91ef1d",
+    "url": "/static/css/2.cd30f7ef.chunk.css"
+  },
+  {
+    "revision": "e47ad35464c7c97a6ba6bc1f52ff26e8",
+    "url": "/index.html"
+  }
+];
