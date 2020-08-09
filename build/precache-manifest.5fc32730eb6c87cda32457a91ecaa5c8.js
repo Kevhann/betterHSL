689c66c8200e9cf6852826dd77017dcdb51e8aae
Tiera Salmi <tiera.salmi@gmail.com>
2020-08-09 14:40:24 +0300

    added a heroku deployment script

---
+++ build/precache-manifest.5fc32730eb6c87cda32457a91ecaa5c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2172a22f5e1ab3cc6dd86cce7c6e0799",
+    "url": "/static/media/banner.2172a22f.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ec245a14a1db4f470e0d",
+    "url": "/static/js/main.0eef9b56.chunk.js"
+  },
+  {
+    "revision": "fddf01aaa5c8452abe4a",
+    "url": "/static/js/2.c6852a61.chunk.js"
+  },
+  {
+    "revision": "ec245a14a1db4f470e0d",
+    "url": "/static/css/main.10fed063.chunk.css"
+  },
+  {
+    "revision": "fddf01aaa5c8452abe4a",
+    "url": "/static/css/2.d92228f8.chunk.css"
+  },
+  {
+    "revision": "2d177878953f52ab55b59b089e39dccc",
+    "url": "/index.html"
+  }
+];
